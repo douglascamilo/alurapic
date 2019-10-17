ee733ecf2b0d44419dd12ef715ffc99102504c4b
Douglas Sousa <douglascamilo@gmail.com>
2019-10-17 11:45:21 -0300

    [Aula 02] - Utilizando diretiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alurapic';
+
+  photos = [
+    {
+      url: 'https://cdn.pixabay.com/photo/2019/07/28/16/23/nissan-4368962_960_720.jpg',
+      description: 'Carro Esportivo'
+    },
+    {
+      url: 'https://cdn.pixabay.com/photo/2019/09/22/16/20/location-4496459_960_720.png',
+      description: 'Location'
+    },
+  ];
 }
