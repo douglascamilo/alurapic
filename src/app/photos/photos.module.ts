61392eaed7780f0d81cca72a06c1b21387126cdf
Douglas Sousa <douglascamilo@gmail.com>
2019-10-18 03:18:29 -0300

    [Aula 04] - Padronização nos imports da aplicação

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 
-import { PhotoComponent } from "./photo/photo.component";
+import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
