37b95ee586fc71045e9c3c72aa2a781625546e7b
Douglas Sousa <douglascamilo@gmail.com>
2019-10-19 09:23:35 -0300

    [Aula 05] - Detectando alterações em PhotosComponent

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { Photo } from '../../photo/photo';
 
@@ -7,21 +7,24 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
   @Input() photos: Photo[] = [];
-  rows: any[] = [];
+  rows: Photo[] = [];
 
   constructor() { }
 
-  ngOnInit() {
-    this.rows = this.groupColumns();
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.groupColumns();
+    }
   }
 
-  private groupColumns() {
-    const newRows = [];
+  private groupColumns(): any[] {
+    const newRows: Photo[] = [];
 
-    for (let index = 0; index < this.photos; index += 3) {
-      newRows.push(this.photos.slice(index, index + 3));
+    for (let index = 0; index < this.photos.length; index += 3) {
+      let items: Photo[] = this.photos.slice(index, index + 3);
+      newRows.push(items);
     }
 
     return newRows;
