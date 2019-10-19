5a1e4ca37961abe4089d49cd5c68ec46d6b992d6
Douglas Sousa <douglascamilo@gmail.com>
2019-10-19 00:44:20 -0300

    [Aula 04] - Rotas com parâmetros

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
@@ -9,16 +10,17 @@ import { Photo } from '../photo/photo';
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
+  photos: Photo[] = [];
 
-  photos: Photo[] = [
-  ];
-
-  constructor(private photoService: PhotoService) {
-  }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const userName = this.activatedRoute.snapshot.params.userName;
+
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe(photos => this.photos = photos);
   }
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 
 import { Photo } from './photo';
 
+const API = 'http://localhost:3000';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +15,6 @@ export class PhotoService {
 
   listFromUser(userName: string) {
     return this.http
-      .get<Photo[]>('http://localhost:3000/flavio/photos');
+      .get<Photo[]>(API + '/' + userName + '/photos');
   }
 }
