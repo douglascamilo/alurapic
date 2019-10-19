d75ade94b0c9e69eba0ee999ba3f30f2b5bd39e6
Douglas Sousa <douglascamilo@gmail.com>
2019-10-19 09:09:52 -0300

    [Aula 05] - Implementando a lógica do Grid

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -9,9 +9,21 @@ import { Photo } from '../../photo/photo';
 })
 export class PhotosComponent implements OnInit {
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.rows = this.groupColumns();
+  }
+
+  private groupColumns() {
+    const newRows = [];
+
+    for (let index = 0; index < this.photos; index += 3) {
+      newRows.push(this.photos.slice(index, index + 3));
+    }
+
+    return newRows;
   }
 }
