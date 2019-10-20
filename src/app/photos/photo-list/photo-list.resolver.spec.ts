c8dc28abe31bd9bdee316de3745750858e40e202
Douglas Sousa <douglascamilo@gmail.com>
2019-10-19 22:59:58 -0300

    [Aula 06] - Implementando Resolvers

---
+++ src/app/photos/photo-list/photo-list.resolver.spec.ts
@@ -0,0 +1,7 @@
+import { PhotoList.Resolver } from './photo-list.resolver';
+
+describe('PhotoList.Resolver', () => {
+  it('should create an instance', () => {
+    expect(new PhotoList.Resolver()).toBeTruthy();
+  });
+});
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { Photo } from './photo';
 
@@ -13,7 +14,7 @@ export class PhotoService {
   constructor(private http: HttpClient) {
   }
 
-  listFromUser(userName: string) {
+  listFromUser(userName: string): Observable<Photo[]> {
     return this.http
       .get<Photo[]>(API + '/' + userName + '/photos');
   }
