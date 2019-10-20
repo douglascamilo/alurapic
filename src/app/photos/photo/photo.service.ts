f21dcf38b61f32d1b0f70c5aef8fb05b71ed3f1f
Douglas Sousa <douglascamilo@gmail.com>
2019-10-19 23:42:56 -0300

    [Aula 06] - Paginação de dados

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Photo } from './photo';
 
-const API = 'http://localhost:3000';
+const URL_PHOTOS = 'http://localhost:3000/:userName/photos';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,23 @@ export class PhotoService {
   }
 
   listFromUser(userName: string): Observable<Photo[]> {
+    const url = this.getUrlPhotos(userName);
+
     return this.http
-      .get<Photo[]>(API + '/' + userName + '/photos');
+      .get<Photo[]>(url);
+  }
+
+  listFromUserPaginated(userName: string, page: number) {
+    const url = this.getUrlPhotos(userName);
+    const params = new HttpParams().append('page', page.toString());
+
+    return this.http
+      .get<Photo[]>(url, {
+        params: params
+      })
+  }
+
+  private getUrlPhotos(userName: string) {
+    return URL_PHOTOS.replace(':userName', userName);
   }
 }
