e425ef971f4816f32204faf47ef03f87f86fe9ef
Douglas Sousa <douglascamilo@gmail.com>
2019-10-21 00:02:45 -0300

    [Aula 02] - Acesso do header de resposta

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const URL = 'http://localhost:3000/user/login';
 
@@ -13,9 +14,16 @@ export class AuthService {
 
   authenticate(userName: string, password: string) {
     return this.http
-      .post(URL, {
-        userName: userName,
-        password: password
-      });
+      .post(
+        URL,
+        {
+          userName: userName,
+          password: password
+        },
+        { observe: 'response'})
+      .pipe(tap(response => {
+        const authToken = response.headers.get('x-access-token');
+        console.log(authToken);
+      }));
   }
 }
