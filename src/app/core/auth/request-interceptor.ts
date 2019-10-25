17b902b5fd9553e989988a23d43cfab5469fac6a
Douglas Sousa <douglascamilo@gmail.com>
2019-10-25 02:28:00 -0300

    [Aula 05] - Interceptadores e envio do token

---
+++ src/app/core/auth/request-interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { TokenService } from '../token/token.service';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+  constructor(
+    private tokenService: TokenService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    req = this.setTokenToHeaderIfLoggedIn(req);
+    return next.handle(req);
+  }
+
+  private setTokenToHeaderIfLoggedIn(req: HttpRequest<any>) {
+    if (this.tokenService.hasToken()) {
+      const token = this.tokenService.getToken();
+
+      req = req.clone({
+        setHeaders: {
+          'x-access-token': token
+        }
+      });
+    }
+    return req;
+  }
+}
